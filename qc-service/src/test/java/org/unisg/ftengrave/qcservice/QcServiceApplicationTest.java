e18c294df570d80f5d12148d4833c31d82be42b1
SerialNo25 <flurin.selm@student.unisg.ch>
2026-03-24 12:15:49 +0100

    feat[QC-Service]: implemented QC service infrastructure

---
+++ qc-service/src/test/java/org/unisg/ftengrave/qcservice/QcServiceApplicationTest.java
@@ -0,0 +1,13 @@
+package org.unisg.ftengrave.qcservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QcServiceApplicationTest {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
