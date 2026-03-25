ba5dfd09a5e9da4062be7950ac41a4565e5a1c47
SerialNo25 <flurin.selm@student.unisg.ch>
2026-03-25 15:40:57 +0100

    feat[sorter-integration-service]: implemented base for factory integrator

---
+++ sorter-integration-setrvice/src/test/java/org/unisg/ftengrave/sorterintegrationsetrvice/SorterIntegrationSetrviceApplicationTest.java
@@ -0,0 +1,12 @@
+package org.unisg.ftengrave.sorterintegrationsetrvice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SorterIntegrationSetrviceApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
